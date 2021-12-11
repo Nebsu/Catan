389b339f167627dee74367dcd0bdb753780480d6
Sebastien Zhou <Sebastien.4084@gmail.com>
2021-12-11 13:35:36 +0100

    Ajout du deck

--- Catan/Card.java
+++ Catan/Card.java
@@ -1,5 +1,16 @@
 package Catan;
 
 class Card {
-    // TODO
+    private String cardname;
+    private int id;
+
+    Card(String cardname, int id){
+        this.cardname = cardname;
+        this.id = id;
+    }
+
+    public String toString(){
+        return this.cardname + " " + this.id;
+    }
+}
 }
